a1ad4f375c94fe0b1aeed5dd5ad0f06a83a027ff
Rubik90 <31125315+Rubik90@users.noreply.github.com>
2020-03-25 11:28:50 +0100

    up

---
+++ library_java/src/main/java/com/is2/theweather/CustomIssueRegistry.java
@@ -0,0 +1,20 @@
+package com.is2.theweather;
+
+
+import com.android.tools.lint.client.api.IssueRegistry;
+import com.android.tools.lint.detector.api.Issue;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CustomIssueRegistry extends IssueRegistry {
+    public CustomIssueRegistry() {
+    }
+
+    @Override
+    public List<Issue> getIssues() {
+        return Arrays.asList(TheWeatherDetector.ISSUE,LintDetector.ISSUE);
+    }
+
+    @Override public int getApi() { return com.android.tools.lint.detector.api.ApiKt.CURRENT_API; }
+}
